af8cfa527a542a84fd4a7ac88c79990b183de2ba
MasterGowen <mastergowen@gmail.com>
2016-04-08 20:05:18 +0600

    django translate

---
+++ django_translate.py
@@ -0,0 +1,14 @@
+import sublime, sublime_plugin
+
+class DjangoTranslateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+
+        translator_begin = "{% trans \'"
+        translator_end = "\' %}"
+        messages = list(self.view.sel())
+
+        for message in messages:
+            self.view.insert(edit, message.end(), translator_end)
+            self.view.insert(edit, message.begin(), translator_begin)
+            
+        print('Added trans blocks.')
